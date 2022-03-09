c5f4f231c1effda5b531e4729be0acf2a28156d8
Nixon <lazaronixon@hotmail.com>
2022-03-09 00:42:08 -0300

    administration:scaffold

--- lib/generators/administration/resource/resource_generator.rb
+++ lib/generators/administration/resource/resource_generator.rb
@@ -1,7 +1,32 @@
+require "rails/generators/resource_helpers"
+
 module Administration
   module Generators
     class ResourceGenerator < Rails::Generators::NamedBase
+      include Rails::Generators::ResourceHelpers
+
+      argument :attributes, type: :array, default: [], banner: "field:type field:type"
+
+      class_option :orm, banner: "NAME", type: :string, required: true, desc: "ORM to generate the controller for"
+
       source_root File.expand_path("templates", __dir__)
+
+      def create_files
+        template "controller.rb", "app/controllers/admin/#{file_name}_controller.rb"
+      end
+
+      private
+        def permitted_params
+          attachments, others = attributes_names.partition { |name| attachments?(name) }
+          params = others.map { |name| ":#{name}" }
+          params += attachments.map { |name| "#{name}: []" }
+          params.join(", ")
+        end
+
+        def attachments?(name)
+          attribute = attributes.find { |attr| attr.name == name }
+          attribute&.attachments?
+        end
     end
   end
 end
