9d27f89156da1a32158c7d71c308b77dc1822de4
Nixon <lazaronixon@hotmail.com>
2022-05-07 21:50:12 -0300

    Refactor validations

--- lib/generators/admin/install/templates/models/admin/user.rb
+++ lib/generators/admin/install/templates/models/admin/user.rb
@@ -1,11 +1,8 @@
 class Admin::User < Admin::ApplicationRecord
   has_secure_password
 
-  validates :email, presence: true, uniqueness: true
-  validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/
-
-  validates_length_of :password, minimum: 12, allow_nil: true
-  validates_format_of :password, with: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/, allow_nil: true, message: "might easily be guessed"
+  validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates :password, allow_nil: true, length: { minimum: 12 }, format: { with: /(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])/ }
 
   before_validation do
     self.email = email.downcase.strip
